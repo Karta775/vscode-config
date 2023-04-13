ef0d546588e9a50cea7076f782bf702ac588fb24
Karta775 <Karta775@users.noreply.github.com>
2023-04-13 03:46:27 +0100

    Further cleanup of install.sh

--- install.sh
+++ install.sh
@@ -3,15 +3,22 @@
 CFG=settings.json
 CODE_OSS=~/.config/Code\ -\ OSS/User
 
-err() { echo $1; exit 1; }
+err() { echo "$1"; exit 1; }
+
+backup() {
+	echo "Backing up old configuration to $1/$CFG.bac"
+	cp -v "$1/$CFG" "$1/$CFG.bac"
+}
+
+link() {
+	echo "Linking new configuration"
+	ln -sfv "$(realpath $CFG)" "$CODE_OSS/$CFG"
+}
 
 [ -L "$CODE_OSS/$CFG" ] && err "You have already installed the configuration!"
 [ ! -e $CFG ] && err "Couldn't find the settings.json file, make sure you cd into the repo"
 
 if [ -d "$CODE_OSS" ]; then
-	echo "Backing up old configuration to $CODE_OSS/$CFG.bac"
-	cp -v "$CODE_OSS/$CFG" "$CODE_OSS/$CFG.bac"
-	[ $? == 0 ] || err "Couldn't back up the old configuration, quitting..."
-	echo "Linking new configuration"
-	ln -sfv $(realpath $CFG) "$CODE_OSS/$CFG"
+	backup "$CODE_OSS" || err "Couldn't back up the old configuration, quitting..."
+	link "$CODE_OSS"
 fi
