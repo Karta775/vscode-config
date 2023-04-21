6b68faf75009d74c61e440de3fb9164f3686f8db
Karta775 <Karta775@users.noreply.github.com>
2023-04-21 15:04:57 +0100

    Added compatibility with macOS

--- install.sh
+++ install.sh
@@ -2,31 +2,41 @@
 
 CFG=settings.json
 SNIP=snippets
+CODE=""
 CODE_OSS=~/.config/Code\ -\ OSS/User
 CODE_MAC=~/Library/Application\ Support/Code/User
 
 err() { echo "$1"; exit 1; }
 
 backup() {
-	echo "Backing up old configuration to $1/$CFG.bac"
-	cp -v "$1/$CFG" "$1/$CFG.bac"
-	[ -d "$1/$SNIP" ] && {
-		echo "Backing up old snippets to $1/$SNIP.bac/"
-		mkdir "$1/$SNIP.bac" && cp -v "$1/$SNIP/*" "$1/$SNIP.bac/"
+	echo "Backing up old configuration to $CODE/$CFG.bac"
+	cp -v "$CODE/$CFG" "$CODE/$CFG.bac"
+	[ -d "$CODE/$SNIP" ] && {
+		echo "Backing up old snippets to $CODE/$SNIP.bac/"
+		mkdir "$CODE/$SNIP.bac" && cp -v "$CODE/$SNIP/*" "$CODE/$SNIP.bac/"
 	}
 }
 
 link() {
 	echo "Linking new configuration"
-	ln -sfv "$(realpath $CFG)" "$CODE_OSS/$CFG"
+	ln -sfv "$(realpath $CFG)" "$CODE/$CFG"
 	echo "Linking new snippets"
-	ln -sfv "$(realpath $SNIP)"/* "$CODE_OSS/$SNIP/"
+	ln -sfv "$(realpath $SNIP)"/* "$CODE/$SNIP/"
 }
 
-[ -L "$CODE_OSS/$CFG" ] && err "You have already installed the configuration!"
+# Detect installation
+if [ -d "$CODE_OSS" ]; then
+	CODE=$CODE_OSS
+elif [ -d "$CODE_MAC" ]; then
+	CODE=$CODE_MAC
+else
+	err "Couldn't find your Visual Studio Code installation"
+fi
+
+# Guards
+[ -L "$CODE/$CFG" ] && err "You have already installed the configuration!"
 [ ! -e $CFG ] && err "Couldn't find the settings.json file, make sure you cd into the repo"
 
-if [ -d "$CODE_OSS" ]; then
-	backup "$CODE_OSS" || err "Couldn't back up the old configuration, quitting..."
-	link "$CODE_OSS"
-fi
+# Install
+backup || err "Couldn't back up the old configuration, quitting..."
+link
